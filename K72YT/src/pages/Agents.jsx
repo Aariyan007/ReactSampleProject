c7425b7097c5477584d6a8e3fadb84e5cecc805c
Aariyan007 <aariyansunu28@gmail.com>
2025-12-14 23:47:05 +0530

    added chnage color bottom

--- K72YT/src/pages/Agents.jsx
+++ K72YT/src/pages/Agents.jsx
@@ -6,15 +6,16 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 const Agents = () => {
   const imageDivfF = useRef(null);
   const imageRef = useRef(null);
+  const section3Ref = useRef(null);
   const imageArray = ["christmas.jpg", "image1.jpg", "image2.jpg", "image3.jpeg", "image4.jpeg", "image5.jpeg"];
-  
+
   gsap.registerPlugin(ScrollTrigger);
-  
+
   useGSAP(function () {
     gsap.to(imageDivfF.current, {
       scrollTrigger: {
         trigger: imageDivfF.current,
-        start: "top 1%",
+        start: "top -10%",
         end: "top -150%",
         pin: true,
         pinSpacing: true,
@@ -34,10 +35,20 @@ const Agents = () => {
         }
       }
     });
-  })
-  
+    gsap.to(section3Ref.current, {
+      backgroundColor: '#ffffff',
+      scrollTrigger: {
+        trigger: section3Ref.current,
+        markers: true,
+        start: "top -320%",
+        end: "top -340%",
+        scrub: true,
+      }
+    })
+  });
+
   return (
-    <div className='parent bg-black'>
+    <div ref={section3Ref} className='parent bg-black'>
       <div className='section1 py-1'>
         <div ref={imageDivfF} className='absolute overflow-hidden h-[50vw] md:h-[40vw] lg:h-[35vw] w-[50vw] md:w-[40vw] lg:w-[35vw] bg-red-600 top-1 left-[50%] -translate-x-1/2 md:left-[20vw] md:translate-x-0 rounded-2xl'>
           <img ref={imageRef} className='h-full w-full object-cover' src="christmas.jpg" alt="Aariyan" />
@@ -53,7 +64,23 @@ const Agents = () => {
           </div>
         </div>
       </div>
-      <div className='section2 h-screen bg-none'>
+      <div className='section2 h-[50vw] flex flex-col'>
+        <div className='h-[30vw] text-2xl  flex flex-row justify-between font-semibold text-white px-[15vw] pt-64 '>
+          <p className=''>EXPERTISE</p>
+          <p className='pr-[20vw]'>STRATERGY<br />
+            BRANDING<br />
+            DIGITAL MARKETING<br />
+            SOCIAL MEDIA MANAGEMENT<br />
+          </p>
+        </div>
+        <div className='h-[20vw] text-xl flex flex-row justify-around pt-28 font-semibold text-white px-12'>
+          <p>Our Work_ Born in curiosity, raised by <br /> dedication and fed with a steady diet of <br />creativity.</p>
+          <p>Our Creative_ Simmering in an <br /> environment where talent can come to a <br /> full boil. Encouraged to become the best <br /> versions of ourselves.</p>
+          <p>Our Culture_ We’re open to <br /> each other. Period. The team <br /> works together to create a <br /> space that makes us proud.</p>
+        </div>
+      </div>
+      <div className='min-h-screen'>
+
       </div>
     </div>
   )
